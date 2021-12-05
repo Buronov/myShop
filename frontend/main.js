d363bf625bfc8b90e3f4682fa9473c6ab92f171b
Buronov <boronovjonibek@gmail.com>
2021-12-05 23:01:01 +0900

    fix: Added Swal sweetalert

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,35 @@
 console.log("Main JS is runing..!")
 import moment from 'moment';
-import momen from 'moment'
+import momen from 'moment';
+import Swal from 'sweetalert2'
 // import $ from 'jquery' 
 
 
 
 $(function () {
+    // ************* STORE PAGE *************************
     $("#button_cont").on('click', () => {
         console.log("you clicced Contact button");
         OpenPopupCenter("/contact", 'contact', 1240, 760)
     });
+    $('#button_author').on('click', () => {
+        console.log("You pressed Author Button");
+        window.open('/author', '_self');
+    });
 
+    $('#button_home').on('click', () => {
+        console.log("You pressed Home Button");
+        Swal.fire({
+            text: 'You Are In Home Page!',
+            icon: 'info',
+            confirmButtonText: 'Ok'
+          });
+    });
+
+
+
+
+    // ************* CONTACT PAGE *************************
     $("#close_btn").on('click', () => {
         console.log("Pressed Close Button");
         window.opener.location.reload();
@@ -19,6 +38,11 @@ $(function () {
 
     $('#submit_btn').on('click', () => {
         console.log("pressed Submit button");
+        Swal.fire({
+            text: 'Message Successfully Submitted',
+            icon: 'success',
+            confirmButtonText: 'thanks!'
+          });
         const name_t = $('#input_name').val();
         const email_t = $('#input_email').val();
         const text_t = $('textarea').val();
@@ -29,7 +53,10 @@ $(function () {
             time: moment().format(),
         };
         console.log(params);
-        window.close();
+        setTimeout(() => {
+            window.close();
+        }, 2000);
+        
 
         $.post('/call-me', params, data => {
             console.log("data : ", data);
